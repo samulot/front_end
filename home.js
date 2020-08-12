e2f24054c922f90fad9f419dcdc0ed3baaa010cd
nilsonjsm <nilsonjsm@gmail.com>
2020-08-12 18:01:42 -0300

    pagina index e home

---
+++ home.js
@@ -0,0 +1,80 @@
+/* qual o objetivo dessa função? 
+   1 - acessar o localStorage e ver se existe um item chamado ScheduleUSER
+       se tiver, recupera, trata e usa as informações para preencher as lacunas dos dados do 
+       usuario
+   2 - se não existir esse item? retorna para o index (sinal que não tem usuário conectado)
+*/
+
+var templateFoto = `<img src="{{LINKFOTO}}" width="100%">`;
+var templateInfo = `Nome: {{NOME}} <br>
+                    Email: {{EMAIL}} <br>
+                    RACF: {{RACF}} <br>
+                    Funcional: {{FUNCIONAL}} <br>
+                    Departamento: {{DEPARTAMENTO}} / {{UNIDADE}}`;
+function carregarInfoUsuario(){
+    var userSTR = localStorage.getItem("ScheduleUSER");
+    if (!userSTR){    // o objeto não existe no Local Storage
+        window.location = "index.html";
+    }
+    else{
+        // vou ter que converter de STRING para Objeto
+        var user = JSON.parse(userSTR);
+
+        document.getElementById("fotoUSER").innerHTML = templateFoto.replace("{{LINKFOTO}}",user.linkFoto);
+
+        var infoUser = templateInfo.replace("{{NOME}}", user.nome)
+                                   .replace("{{EMAIL}}", user.email)
+                                   .replace("{{RACF}}",  user.racf)        
+                                   .replace("{{FUNCIONAL}}", user.funcional)
+                                   .replace("{{DEPARTAMENTO}}", user.depto.nome)    
+                                   .replace("{{UNIDADE}}", user.depto.unidade);
+        document.getElementById("infoUSER").innerHTML = infoUser;  
+        
+        // agora vou carregar os dados da agencia
+        carregaAgencias();
+    }
+
+
+}
+
+    
+
+function carregaAgencias(){
+   fetch("http://localhost:8088/agencias")
+    .then(res => res.json())
+    .then(listaAgencias => preencheComboBox(listaAgencias)); 
+}
+
+function preencheComboBox(listaAgencias){
+    var templateSelect = `<select class="form-control" id="selectAg"> {{OPCOES}} </select> `;
+    var templateOption = `<option value="{{VALOR}}"> {{NOME}} </option>`;
+    
+    var opcoes = "";
+    for (i=0; i<listaAgencias.length; i++){
+        var ag = listaAgencias[i];
+        opcoes = opcoes + templateOption.replace("{{VALOR}}", ag.id)
+                                        .replace("{{NOME}}", ag.nome);
+    }
+    var novoSelect = templateSelect.replace("{{OPCOES}}", opcoes);
+    document.getElementById("optionAgencia").innerHTML = novoSelect;
+}
+
+function gerarRelatorio(){
+    // para saber se tá todo mundo "checado"
+    var combinacao = 0;
+    if (document.getElementById("selectAgencia").checked){
+        combinacao = combinacao + 1;
+    } 
+    if (document.getElementById("selectData").checked){
+        combinacao = combinacao + 2;
+    } 
+    if (document.getElementById("selectCliente").checked){
+        combinacao = combinacao + 4;
+    } 
+    console.log("Combinacao = "+combinacao);
+   
+    var op = document.getElementById("selectAg");
+    console.log(op.options[op.selectedIndex].value);
+    console.log(document.getElementById("txtData").value);
+    console.log(document.getElementById("txtCliente").value);
+}
---
+++ index.js
@@ -0,0 +1,66 @@
+function autenticar(){
+    //recupero os valores digitados no campo de INPUT
+    var txtEmail = document.getElementById("txtEmail").value;
+    var txtSenha = document.getElementById("txtSenha").value;
+    // vou exibi-los no console
+    console.log("Digitou =" +txtEmail+ " / "+txtSenha);
+
+    // definir alguns passos
+
+    // essa é a mensagem do corpo
+
+    var msgBody = {
+        email : txtEmail,
+        senha : txtSenha
+    }
+
+    console.log(msgBody)
+    // preciso, agora, definir o formato da mensagem de cabeçalho
+    // 1 - definir o método como POST
+    // 2 - definir a mensagem como corpo da requisição (só que convertido para STRING)
+    // 3 - definir que a string é do tipo Application/JSON
+    var cabecalho = {
+        method : "POST",
+        body : JSON.stringify(msgBody),   // preciso converter o objeto da msgBody para uma STRING JSON
+        headers : {
+            "Content-type":"application/json"            
+        }
+
+    }
+
+    // agora sim posso enviar os dados
+    // então funciona assim: lançamos a solicitação FETCH para o backEnd
+    // como não é algo síncrono, temos que esperar pelo resultado. Assim que vier o resultado, 
+    // armazenamos numa variável chamada RES. E, chegando esse RES, chamamos a função trataResposta
+    // passando o RES como parâmetro
+    
+    fetch("http://localhost:8088/login", cabecalho)
+    .then (res => trataResposta(res))
+
+}
+function trataResposta(res){
+    if (res.status==200){
+
+        // document.getElementById("msgERRO").innerHTML = "<h3>Logado com Sucesso</h3>";
+        // do resultado positivo (ou seja, o usuário existe), vou extrair o corpo da mensagem
+        // que veio como JSON e chamar a função LOGAR passando esse objeto JSON
+        res.json().then(objeto => logar(objeto));   
+    }
+    else if (res.status==401){
+        document.getElementById("msgERRO").innerHTML = "<h3>Senha Invalida</h3>";
+    }
+    else if(res.status==404){
+        document.getElementById("msgERRO").innerHTML = "<h3>Usuario Invalido</h3>"
+    }
+}
+
+function logar(objeto){
+    // agora a idéia é armazenar o objeto do usuário recebido na resposta dentro do
+    // armazenamento interno do browser
+
+    // em um sistema real eu não armazeno o objeto, mas um TOKEN
+
+    var objSTR = JSON.stringify(objeto);           // converti o objeto pra uma string
+    localStorage.setItem("ScheduleUSER",objSTR);   // armazenei no local storage
+    window.location = "home.html";                 // vou pra página principal
+}
